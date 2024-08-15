3332c181cedbbed96e6c2e3d9f7f61dba1649b85
Abhinav <abhiasthana76@gmail.com>
2024-08-15 12:21:44 +0530

    Get endpoint

--- news_retriever.py
+++ news_retriever.py
@@ -7,6 +7,32 @@ class REQUEST: # Making requests
     def __init__(self):
         self.getreq = 'https://newsapi.org/' # private get request 
         self.apikey = '2c0c6a594305428aa1aea424a5e9d745' # private news api key
+        # self.endpoint = None
+    def welcome(self):
+        pass
+
+class Endpoint(REQUEST): # Retrieve news with title
+    def __init__(self):
+        super().__init__()
+        self.choice = int(input("Select from below types of headlines: \n\t1. Everything\n\t2. Top Headlines\n Your choice (1-2): "))
+     
+    def endpoint(self):
+        if self.choice == 1:
+            self.type = '/v2/everything'
+        elif self.choice == 2:
+            self.type = '/v2/top-headlines'
+
+        self.req = self.getreq + self.type + '?q=tesla&from=2024-07-15&sortBy=publishedAt&apiKey=' + self.apikey 
+        self.response = requests.get(self.req)
+        # print(self.test)
+        print(self.response.text)
+
+# main
+if __name__ == "__main__":
+    test = Endpoint()
+    test.endpoint()
+
+
 
 
 
