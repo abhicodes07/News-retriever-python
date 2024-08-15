f525fe0a6f6afe92118cb7cd7ae6b17f00369398
Abhinav <abhiasthana76@gmail.com>
2024-08-15 12:20:52 +0530

    Request class

--- news_retriever.py
+++ news_retriever.py
@@ -1,11 +1,12 @@
 import requests
 
 # Exampler response with all endpoints and headers
-response = requests.get('https://newsapi.org/v2/everything?q=apple&from=2024-08-14&to=2024-08-14&sortBy=popularity&apiKey=2c0c6a594305428aa1aea424a5e9d745')
-print(response.text)
+# response = requests.get('https://newsapi.org/v2/everything?q=apple&from=2024-08-14&to=2024-08-14&sortBy=popularity&apiKey=2c0c6a594305428aa1aea424a5e9d745')
 
-
-
+class REQUEST: # Making requests
+    def __init__(self):
+        self.getreq = 'https://newsapi.org/' # private get request 
+        self.apikey = '2c0c6a594305428aa1aea424a5e9d745' # private news api key
 
 
 
